16219111951430247b1c595971f533f49242362b
irisarts <150019972+irisarts@users.noreply.github.com>
2024-07-24 09:45:46 +0200

    first commit

---
+++ html.php
@@ -0,0 +1,28 @@
+<?php
+
+function startHTML() {
+    echo '<!DOCTYPE html>
+        <html>';
+}
+
+function startBody() {
+    echo '<body>
+        <div class="wrapper">
+        <header>Bike Total <i>- A new way of biking</i></header>';
+}
+
+function showHeader(string $title, string $author, string $stylesheet) {
+    echo '<head>
+        <title>'.$title.'</title>
+        <meta name="author" content="'.$author.'" />
+        <link rel="stylesheet" href="'.$stylesheet.'" />
+        </head>';
+}
+
+function showMainContent() {
+    echo '<main>';
+
+    switch ($page) {
+
+    }
+}
